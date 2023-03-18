d0cad72a199d821827020936a7124faeaad5ca5a
del <del@DESKTOP-8I9NF5H>
2023-03-18 13:36:46 +0500

    Add project files.

---
+++ Ikkinchi dars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter celsius degree : ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        Console.Write("Entered celsius degree's  KELVIN quantity is : ");
+        Console.WriteLine(celsius + 273.15);
+
+        Console.Write("Entered celsius degree's  FAHRENHEIT quantity is : ");
+        Console.WriteLine(celsius * 1.8 + 32);
+
+        Console.WriteLine("=============================================");
+        Main(args);
+    }
+}
